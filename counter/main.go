371a8ae3234ef188ba630d3846b68e1e17470342
Manoj Parvathaneni <dscatwork@outlook.com>
2025-03-25 21:29:42 -0400

    Updated counter to persist the counter to file.

--- counter/main.go
+++ counter/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 )
 
 var (
-	Counter = make(map[string]int)
-	mu      sync.Mutex
+	Counter  = make(map[string]int)
+	mu       sync.Mutex
+	filename = "counter.json"
 )
+
+func init() {
+	Counter = readCounter(filename)
+}
 
 func numOfVisits(w http.ResponseWriter, req *http.Request) {
 	mu.Lock()
@@ -19,11 +26,44 @@ func numOfVisits(w http.ResponseWriter, req *http.Request) {
 	mu.Unlock()
 
 	response := map[string]int{"visited": visitedCount}
-
+	persistCounter(response)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(response)
+}
+
+func persistCounter(counter map[string]int) {
+	jsonData, err := json.MarshalIndent(counter, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+
+	// Write JSON data to file
+	err = os.WriteFile(filename, jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON to file:", err)
+		return
+	}
+}
+
+func readCounter(filename string) (counter map[string]int) {
+	// Read JSON data from file
+	readData, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return nil
+	}
+
+	// Convert JSON back to map
+	var readMap map[string]int
+	err = json.Unmarshal(readData, &readMap)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return nil
+	}
+	return readMap
 }
 
 func main() {
